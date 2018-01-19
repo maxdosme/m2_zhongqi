63ed445185795233765429ca5a0533cbc0c55b7e
maxdosme <233705835@qq.com>
2018-01-19 10:27:27 +0800

    '添加注册公司页'

---
+++ src/components/page/zc.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+        <section>
+            <h1>This is 注册公司页</h1>
+            <router-link to="/" tag="h2" style="margin-top: 60px;">首页</router-link>
+        </section>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
