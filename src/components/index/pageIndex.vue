c887c2001a8443fab6d7d1c8acc0a190b689e76a
maxdosme <233705835@qq.com>
2018-01-23 17:04:55 +0800

    '制作注册公司板块样式'

--- src/components/index/pageIndex.vue
+++ src/components/index/pageIndex.vue
@@ -1,90 +1,28 @@
 <template>
-    <el-row>
-      <el-col :span="24">
-          <div class="banner">
-              <img src="static/image/banner.png" alt="banner">
-          </div>
-      </el-col>
-      <el-col :span="24">
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-          <div>内容填充</div>
-      </el-col>
-    </el-row>
+    <div>
+        <el-row>
+            <el-col :span="24">
+                <div class="banner">
+                    <img src="static/image/banner.png" alt="banner">
+                </div>
+            </el-col>
+        </el-row>
+        <div class="index_cont1">
+            <el-row>
+                <el-col :span="21" :offset="3">
+                    <div class="cont1_box">
+                        <i class="icon iconfont icon-wpml"></i>
+                        <h3>注册公司流程</h3>
+                        <p>在线注册公司，简单看得见</p>
+                        <div class="cont1_right">
+                            <i class="icon iconfont icon-you"></i>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="3"></el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -97,4 +35,29 @@ export default {}
         background-color #fff
         img 
             width: 100%
+    .index_cont1
+        position relative
+        height 7.3333rem
+        padding-top 2rem
+        .cont1_box
+            position relative
+            & .icon-wpml
+                float left
+                font-size 6.5rem
+                margin-right .5rem
+            & h3
+                float left
+                font-size 2.4rem
+                line-height 3rem
+            & p
+                float left
+                font-size 1.4rem
+                line-height 3rem
+            .cont1_right
+                position absolute
+                right 1rem
+                float right
+                & .icon-you
+                    font-size 3rem
+                    line-height 5rem
 </style>
